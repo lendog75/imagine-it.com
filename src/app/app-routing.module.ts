20f55f87e6f899d128acefa38dd381516188157b
Leonard Reed <leonardreed@Leonards-MacBook-Pro.local>
2018-09-29 18:02:03 -0600

    more logo tweaks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,34 +7,20 @@ import { TechStackComponent } from './tech-stack/tech-stack.component';
 import { OurStoryDetailComponent } from './about/our-story-detail/our-story-detail.component';
 
 const appRoutes: Routes = [
-
     { path: 'contact', component: ContactUsComponent },
     { path: 'blog', component: BlogComponent },
     { path: 'our-tech', component: TechStackComponent },
     { path: 'our-story', component: OurStoryDetailComponent },
     { path: 'home', component: HomeComponent },
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
-    //{ path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
     imports: [
         RouterModule.forRoot(appRoutes)
-        //RouterModule.forRoot(
-        //    appRoutes,
-        //    { enableTracing: true } // <-- debugging purposes only
-        //)
     ],
     exports: [
         RouterModule
     ]
 })
 export class AppRoutingModule {}
-
-//
-//const appRoutes: Routes = [
-//    { path: 'crisis-center', component: CrisisListComponent },
-//    { path: 'heroes',        component: HeroListComponent },
-//    { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
-//    { path: '**', component: PageNotFoundComponent }
-//];
